170986c79c2defda75cc843caab01baa7c0664a9
Brett Lischalk <blischalk@opinionlab.com>
2017-08-30 14:57:16 -0500

    Read data files from disk

--- spec/file_parse_service/core_spec.clj
+++ spec/file_parse_service/core_spec.clj
@@ -2,8 +2,17 @@
   (:require [speclj.core :refer :all]
             [file-parse-service.core :refer :all]))
 
-(describe "Truth"
-  (it "is true"
-    (should true))
-  (it "is not false"
-    (should-not false)))
+(def data-file-count 3)
+
+(describe 'read-data-files
+  (context "when data files exist"
+    (it "returns aggregate data from data files"
+      (should-not-be empty? (read-data-files)))
+    (it "returns a vector for each file"
+      (should= data-file-count (count (read-data-files)))))
+
+  (context "when data files do not exist"
+    (with-stubs)
+    (it "returns an empty vector"
+      (with-redefs [clojure.core/file-seq (stub :file {:return []})]
+        (should= [] (read-data-files))))))
--- src/file_parse_service/core.clj
+++ src/file_parse_service/core.clj
@@ -1,5 +1,16 @@
 (ns file-parse-service.core
+  (:require [clojure.pprint :refer [pprint]])
   (:gen-class))
+
+(def data-dir "resources")
+
+(defn read-data-files []
+  (let [fs (file-seq (clojure.java.io/file data-dir))
+        files (rest fs)
+        data-files (if-not files
+                     []
+                     (map slurp files))]
+    data-files))
 
 (defn -main
   "I don't do a whole lot ... yet."
