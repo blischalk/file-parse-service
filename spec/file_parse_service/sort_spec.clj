9baf998b2789429ed802c3f70ee3e8d11a5aaa2c
Brett Lischalk <blischalk@opinionlab.com>
2017-08-31 14:52:53 -0500

    Complete sorting specifics

--- spec/file_parse_service/sort_spec.clj
+++ spec/file_parse_service/sort_spec.clj
@@ -3,7 +3,7 @@
             [file-parse-service.parse :refer [row->person]]
             [file-parse-service.sort :refer :all]))
 
-(def p1 (row->person ["Doe" "Jane" "Female" "Blue" "11-28-1983"]))
+(def p1 (row->person ["Ryan" "Jane" "Female" "Blue" "11-28-1983"]))
 (def p2 (row->person ["Daniel" "John" "Male" "Blue" "10-28-1923"]))
 (def p3 (row->person ["Marley" "Bob" "Male" "Green" "02-10-1945"]))
 (def p4 (row->person ["McDonald" "Ronda" "Female" "Pink" "11-06-1953"]))
@@ -11,7 +11,8 @@
 
 (describe 'sort-by-field
   (it "sorts by last name decending"
-    (should= [p4 p3 p1 p2] (sort-by-field records "lname")))
+    (should= [p1 p4 p3 p2] (sort-by-field records "lname")))
   (it "sorts by birth date ascending"
     (should= [p2 p3 p4 p1] (sort-by-field records "dob")))
-  (it "sorts by gender (females before males), last name ascending"))
+  (it "sorts by gender (females before males), last name asc"
+    (should= [p4 p1 p2 p3] (sort-by-field records "sex"))))
--- src/file_parse_service/sort.clj
+++ src/file_parse_service/sort.clj
@@ -1,9 +1,21 @@
 (ns file-parse-service.sort)
 
+(declare sort-by-field)
+
+(defn sort-by-sex
+[data]
+(let [females (filter (fn [r] (= (:sex r) "Female")) data)
+      males (filter (fn [r] (= (:sex r) "Male")) data)
+      sorted-females (reverse (sort-by-field females "lname"))
+      sorted-males (reverse (sort-by-field males "lname"))]
+  (concat sorted-females sorted-males)))
+
+
 (defn sort-by-field
   "Keywordize sort field and sort data by it"
   [data field]
   (let [sbk (partial sort-by (keyword field))]
     (case field
       "lname" (reverse (sbk data))
+      "sex" (sort-by-sex data)
       (sbk data))))
