773a627cb291a178a766d4881a95f41d8c14ebd8
Brett Lischalk <blischalk@opinionlab.com>
2017-08-31 22:44:35 -0500

    Adding more tests for sorting

--- spec/file_parse_service/sort_spec.clj
+++ spec/file_parse_service/sort_spec.clj
@@ -9,4 +9,10 @@
   (it "sorts by birth date ascending"
     (should= [p2 p3 p4 p1] (sort-by-field records "dob")))
   (it "sorts by gender (females before males), last name asc"
-    (should= [p4 p1 p2 p3] (sort-by-field records "gender"))))
+    (should= [p4 p1 p2 p3] (sort-by-field records "gender")))
+  (it "handles gracefully with a non-existent field"
+    (should-not-throw (sort-by-field records "blahblah")))
+  (it "handles gracefully an empty records collection"
+    (should-not-throw (sort-by-field [] "blahblah")))
+  (it "handles gracefully a nil records collection"
+    (should-not-throw (sort-by-field nil "blahblah"))))
