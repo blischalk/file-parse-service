2643295a863c0ec4bf09fcb87bacc23f5b9cf174
Brett Lischalk <blischalk@opinionlab.com>
2017-08-31 22:29:52 -0500

    Removing test directory as speclj is used

--- test/file_parse_service/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns file-parse-service.core-test
-  (:require [clojure.test :refer :all]
-            [file-parse-service.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
